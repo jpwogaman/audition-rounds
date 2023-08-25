a60eef124df3f83c9b6b421fe1fa8a0c794fae80
JP Wogaman II <jpwogaman@gmail.com>
2023-08-25 12:48:25 -0400

    formatting, function declarations, os import to remove .txt file after printing

--- print_test.py
+++ print_test.py
@@ -1,39 +1,39 @@
-"""prints a document to the default printer"""
+"""Randomly selects excerpts for mock audition rounds, prints 1 page per round to default printer"""
 
 import random
 import tempfile
 from typing import List
+import os
 import win32api
 import win32print
 
-
-def dev_check():
-    """print console if dev, print printer if prod"""
-    return False
-
-
 def main():
     """entry point"""
-    determine_rounds()
+    rounds = int(input("How many rounds? "))
+    determine_rounds(rounds)
 
+def dev_check():
+    """print console if dev/true, print printer if prod/false"""
+    return False
 
 def number_of_excerpts(round_number):
-    """randomly determines the number of excerpts in a given round"""
+    """determines the number of excerpts in a given round, with more excerpts in later rounds"""
+
     if round_number == 0:
         return random.randint(4, 6)
     if 0 < round_number <= 3:
         return random.randint(5, 9)
     return random.randint(6, 12)
 
-
-def determine_rounds():
-    """randomly determines the order of excerpts for each round"""
-
-    rounds = int(input("How many rounds? "))
+def determine_rounds(rounds):
+    """randomly determines the order of excerpts for each round, then prints them"""
 
     for i in range(rounds):
 
-        selected_excerpts: List[str] = []
+        number_of_excerpts_this_round = number_of_excerpts(i)
+        selected_excerpts: List[str] = [f"Round {i+1}:\n"]
+        if dev_check():
+            print(f"Round {i+1}:\n")
 
         list_of_excerpts = [
             "Respighi, Pines of Rome",
@@ -61,44 +61,33 @@ def determine_rounds():
             "Williams, Hymn to the Fallen, Pt. 1"
         ]
 
-        number_of_excerpts_this_round = number_of_excerpts(i)
-
-        selected_excerpts.append(f"Round {i+1}:\n")
-
-        if dev_check():
-            print(f"Round {i+1}:\n")
-
         for j in range(number_of_excerpts_this_round):
             excerpt = random.choice(list_of_excerpts)
-
             selected_excerpts.append(f"{j+1}. {excerpt}")
-
+            list_of_excerpts.remove(excerpt)
             if dev_check():
                 print(f"{j+1}. {excerpt}")
-
-            list_of_excerpts.remove(excerpt)
 
         selected_excerpts.append("\n")
 
         if dev_check():
             print("\n")
-
-        if dev_check() is False:
+        else:
             txt_file = create_file("\n".join(selected_excerpts))
             print_action(txt_file)
 
-
 def create_file(audition_rounds):
     """creates a .txt file with the audition round information"""
+
     filename = tempfile.mktemp(  # NOSONAR @SuppressWarnings("python:S5445")
         ".txt")
     with open(filename, "w", encoding='utf-8') as file:
         file.write(audition_rounds)
     return filename
 
-
 def print_action(txt_file):
     """actually prints the file"""
+
     win32api.ShellExecute(  # pylint: disable=c-extension-no-member
         0,
         "print",
@@ -108,6 +97,7 @@ def print_action(txt_file):
         0
     )
 
+    os.remove(txt_file)
 
 if __name__ == "__main__":
     main()
